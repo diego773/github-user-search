e6be4640ab76e3bb72614a1a4ca5b0954ca83bb7
Diego <diegovillarreal123@gmail.com>
2023-04-25 20:02:39 -0500

    added fetch and display users info and also use my github username as default

--- script.js
+++ script.js
@@ -1,53 +1,140 @@
-let searchButton = document.querySelector(".search-button");
-let form = document.getElementById("form");
+const searchButton = document.querySelector(".search-button");
+const form = document.getElementById("form");
+const searchInput = document.getElementById("search-input");
 
-form.addEventListener("submit", function (e) {
+const formSubmitHandler = (e) => {
   e.preventDefault();
-  let searchInput = document.getElementById("search-input").value;
-  // let user = searchInput.split(" ").join("");
-  // alert(user);
 
-  fetch("https://api.github.com/users/" + searchInput)
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-      // document.getElementById(
-      //   "user-avatar-container"
-      // ).innerHTML = `<img src="${data.avatar_url}"/>`;
-      const userAvatarImage = document.getElementById("user-card-img");
-      userAvatarImage.src = data.avatar_url;
+  let username = searchInput.value.trim();
 
-      const userName = document.getElementById("user-name");
-      userName.textContent = data.name;
+  if (!username) {
+    alert("Please enter github username");
+  } else {
+    fetchUserApi(username);
 
-      const userEmail = document.getElementById("user-email");
-      userEmail.textContent = data.email;
+    searchInput.value = " ";
+  }
+};
 
-      const dateJoined = document.getElementById("user-date");
-      dateJoined.textContent = data.created_at;
+const fetchUserApi = (username) => {
+  let apiUrl = "https://api.github.com/users/" + username;
 
-      const userBio = document.getElementById("user-notes");
-      userBio.textContent = data.bio;
+  fetch(apiUrl).then((response) => {
+    if (response.ok) {
+      response.json().then((data) => {
+        console.log(data);
+        updateDom(data);
+      });
+    } else {
+      alert("error" + response.statusText);
+    }
+  });
+};
 
-      const repos = document.getElementById("repos");
-      repos.textContent = data.public_repos;
+const updateDom = (user) => {
+  if (!user) {
+    alert("no data");
+    return;
+  }
+  userContainerInfo(user);
+};
 
-      const followers = document.getElementById("followers");
-      followers.textContent = data.followers;
+const userContainerInfo = (user) => {
+  if (!userCardHeader) {
+    user.textContent = " ";
+    return;
+  }
 
-      const following = document.getElementById("following");
-      following.textContent = data.following;
+  if (!userBioContainer) {
+    user.textContent = " ";
+    return;
+  }
 
-      const location = document.getElementById("location");
-      location.textContent = data.location;
+  if (!showReposFollowers) {
+    user.textContent = " ";
+    return;
+  }
 
-      const twitter = document.getElementById("twitter");
-      twitter.textContent = data.twitter_username;
+  if (!userInfoFooter) {
+    user.textContent = " ";
+    return;
+  }
 
-      const blog = document.getElementById("blog");
-      blog.textContent = data.blog;
+  userCardHeader(user);
+  userBioContainer(user);
+  showReposFollowers(user);
+  userInfoFooter(user);
+};
 
-      const company = document.getElementById("company");
-      company.textContent = data.company;
-    });
-});
+const userCardHeader = (user) => {
+  const userAvatarImage = document.getElementById("user-card-img");
+  const userName = document.getElementById("user-name");
+  const userEmail = document.getElementById("user-email");
+  const dateJoined = document.getElementById("user-date");
+
+  if (!user.name) {
+    userName.textContent = user.login;
+    return;
+  }
+  userAvatarImage.src = user.avatar_url;
+
+  userName.textContent = user.name;
+
+  userEmail.textContent = `@${user.login}`;
+
+  dateJoined.textContent = user.created_at;
+};
+
+const userBioContainer = (user) => {
+  const userBio = document.getElementById("user-notes");
+
+  if (!user.bio) {
+    userBio.textContent = "No bio";
+    return;
+  }
+
+  userBio.textContent = user.bio;
+};
+
+const showReposFollowers = (user) => {
+  const repos = document.getElementById("repos");
+  const followers = document.getElementById("followers");
+  const following = document.getElementById("following");
+
+  if (!user) {
+    alert("no data");
+    return;
+  }
+
+  repos.textContent = user.public_repos;
+
+  followers.textContent = user.followers;
+
+  following.textContent = user.following;
+};
+
+const userInfoFooter = (user) => {
+  const location = document.getElementById("location");
+  const twitter = document.getElementById("twitter");
+  const blog = document.getElementById("blog");
+  const company = document.getElementById("company");
+
+  !user.company
+    ? (company.textContent = "Not Available")
+    : (company.textContent = user.company);
+
+  !user.location
+    ? (location.textContent = "Not Available")
+    : (location.textContent = user.location);
+
+  !user.twitter_username
+    ? (twitter.textContent = "Not Available")
+    : (twitter.textContent = user.twitter_username);
+
+  !user.blog
+    ? (blog.textContent = "Not Available")
+    : (blog.textContent = user.blog);
+};
+
+fetchUserApi("diego773");
+form.addEventListener("submit", formSubmitHandler);
